f05c9a085f8fb283191fd17df235e8b0bf4d1fc6
megamilkx <gyrocopter3k@gmail.com>
2017-11-13 03:57:13 +0300

    Add scene object

---
+++ scene_object.h
@@ -0,0 +1,114 @@
+#ifndef SCENE_OBJECT_H
+#define SCENE_OBJECT_H
+
+#include <vector>
+#include <map>
+
+#include "typeindex.h"
+
+#include "core_interface.h"
+
+#undef GetObject
+
+class SceneObject
+{
+friend CoreInterface;
+public:
+    class Component
+    {
+    friend SceneObject;
+    public:
+        virtual ~Component() {}
+        virtual void OnCreate() {}
+        SceneObject* GetObject() { return object; }
+        template<typename T>
+        T* GetComponent()
+        { return GetObject()->GetComponent<T>(); }
+    private:
+        SceneObject* object;
+    };
+    
+    SceneObject() : parentObject(0) {}
+    SceneObject(SceneObject* parent) : parentObject(parent) {}
+    ~SceneObject()
+    {
+        for(unsigned i = 0; i < objects.size(); ++i)
+            delete objects[i];
+    }
+    
+    SceneObject* Root()
+    {
+        if(!parentObject)
+            return this;
+        else
+            return parentObject->Root();
+    }
+    
+    bool IsRoot()
+    {
+        return this == Root();
+    }
+    
+    SceneObject* CreateObject()
+    {
+        SceneObject* o = new SceneObject(this);
+        objects.push_back(o);
+        return o;
+    }
+    template<typename T>
+    T* GetComponent()
+    {
+        T* c = FindComponent<T>();
+        if (!c)
+        {
+            c = new T();
+            c->object = this;
+            components.insert(std::make_pair(TypeInfo<T>::Index(), c));
+            c->OnCreate();
+            return c;
+        }
+        else
+            return c;
+    }
+    
+    template<typename T>
+    T* FindComponent()
+    {
+        std::map<typeindex, Component*>::iterator it;
+        it = components.find(TypeInfo<T>::Index());
+        if(it == components.end())
+            return 0;
+        else
+            return (T*)it->second;
+    }
+    
+    void Name(const std::string& name) { this->name = name; }
+    std::string Name() const { return name; }
+    
+    SceneObject* FindObject(const std::string& name)
+    {
+        SceneObject* o = 0;
+        for(unsigned i = 0; i < objects.size(); ++i)
+        {
+            if(objects[i]->Name() == name)
+            {
+                o = objects[i];
+                break;
+            }
+            else if(o = objects[i]->FindObject(name))
+            {
+                break;
+            }
+        }
+        return o;
+    }
+private:
+    std::string name;
+    SceneObject* parentObject;
+    std::vector<SceneObject*> objects;
+    std::map<typeindex, Component*> components;
+
+    CoreInterface* core;
+};
+
+#endif
